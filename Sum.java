4a65e10fe22b7a98b945da9ed6791d603f7aa108
suraj waykar <surajwaykar24@gmail.com>
2023-07-25 08:43:01 +0530

    sum commited

---
+++ Sum.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Sum{
+    public static void main(String[] args) {
+         Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Input first number: ");
+        double x = sc.nextDouble();
+        
+        System.out.print("Input second number: ");
+        double y = sc.nextDouble();
+        
+        System.out.print("Input third number: ");
+        double z = sc.nextDouble();
+        
+        double largest = x;
+        if (y > largest) {
+            largest = y;
+        }
+        if (z > largest) {
+            largest = z;
+        }
+        
+        double sum = largest;
+        System.out.println("The largest number is " + largest);
+        System.out.println("The sum of the largest number is " + sum);
+    }
+} 
